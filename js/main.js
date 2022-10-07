9dde3443b834d2c06468736dadcf705a9193c2ce
Devang <devngc@gmail.com>
2022-10-07 08:41:57 -0400

    style(*): basic poster adding working

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const searchBtn = document.getElementById("search");
+const mainEl = document.getElementById("posters");
+
+
+
+searchBtn.addEventListener("click", () => {
+    mainEl.style.height = '100%'; 
+    mainEl.innerHTML = `
+    
+    <div>
+        <img src="./images/posters/pos_1.png" class="poster">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et netus et malesuada fames ac turpis egestas. Vulputate mi sit amet mauris commodo quis.</p>
+    </div>
+
+    <div>
+        <img src="./images/posters/pos_2.png" class="poster">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et netus et malesuada fames ac turpis egestas. Vulputate mi sit amet mauris commodo quis.</p>
+    </div>
+
+    <div>
+        <img src="./images/posters/pos_3.png" class="poster">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et netus et malesuada fames ac turpis egestas. Vulputate mi sit amet mauris commodo quis.</p>
+    </div>
+
+    <div>
+        <img src="./images/posters/pos_4.png" class="poster">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et netus et malesuada fames ac turpis egestas. Vulputate mi sit amet mauris commodo quis.</p>
+    </div>
+    `;
+});
+
