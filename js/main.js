a4808b988ccdbce6dd24a7c1796c2715d12d6118
Devang <devngc@gmail.com>
2022-10-07 08:43:54 -0400

    chore(*): reduce poster size

--- js/main.js
+++ js/main.js
@@ -8,22 +8,22 @@ searchBtn.addEventListener("click", () => {
     mainEl.innerHTML = `
     
     <div>
-        <img src="./images/posters/pos_1.png" class="poster">
+        <img src="./images/posters/pos_1.jpg" class="poster">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et netus et malesuada fames ac turpis egestas. Vulputate mi sit amet mauris commodo quis.</p>
     </div>
 
     <div>
-        <img src="./images/posters/pos_2.png" class="poster">
+        <img src="./images/posters/pos_2.jpg" class="poster">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et netus et malesuada fames ac turpis egestas. Vulputate mi sit amet mauris commodo quis.</p>
     </div>
 
     <div>
-        <img src="./images/posters/pos_3.png" class="poster">
+        <img src="./images/posters/pos_3.jpg" class="poster">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et netus et malesuada fames ac turpis egestas. Vulputate mi sit amet mauris commodo quis.</p>
     </div>
 
     <div>
-        <img src="./images/posters/pos_4.png" class="poster">
+        <img src="./images/posters/pos_4.jpg" class="poster">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et netus et malesuada fames ac turpis egestas. Vulputate mi sit amet mauris commodo quis.</p>
     </div>
     `;
