6c0f795e1988be1e0397dcc4d7ba1dbaa4ec399a
Devang <devngc@gmail.com>
2022-10-15 16:40:45 -0400

    fix(*): basic localstorage operation working

--- js/watchlist.js
+++ js/watchlist.js
@@ -1,6 +1,24 @@
-const addMoviesBtn = document.getElementById("add-movies");
+import Movie from "./Movie.js";
 
-console.log(addMoviesBtn);
+const addMoviesBtn = document.getElementById("add-movies");
+const moviesEl = document.getElementById("movies");
+const movies = JSON.parse(localStorage.movies);
+
 addMoviesBtn.addEventListener("click", () => {
     window.location="index.html";
 });
+
+
+
+
+function movieObject(movie) {
+    const genre = movie.genre.split(',');
+    movie.genre = genre;
+    return new Movie(movie).getHTML();
+}
+
+if (movies.length > 0) {
+    moviesEl.style.height = '100%'; 
+    moviesEl.innerHTML = movies.map(movieObject).join(' ');
+}
+
