281dc4abfa1bf4e19414f33e51b7c6fd1b25935d
Devang <devngc@gmail.com>
2022-10-16 21:18:44 -0400

    fix(*): fully working version

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 
 import Movie from './Movie.js';
-import movies from './data.js';
 
 
 const searchBtn = document.getElementById("search");
 const moviesEl = document.getElementById("movies");
+const inputEl = document.getElementById("input");
 
 
 if (localStorage.getItem('movies') === null) {
@@ -21,9 +21,62 @@ function updateLocalStorage(movie) {
     }
 }
 
-searchBtn.addEventListener("click", () => {
-    moviesEl.style.height = '100%'; 
-    moviesEl.innerHTML = movies.map(movie => new Movie(movie).getHTML()).join(' ');
+
+
+function addMovieToDom(movie) {
+    const movieObj = {
+        'title': movie.Title,
+        'src': movie.Poster,
+        'stars': movie.imdbRating,
+        'length': movie.Runtime,
+        'genre': movie.Genre,
+        'description': movie.Plot
+    };
+
+    
+    moviesEl.innerHTML += new Movie(movieObj).getHTML();
+}
+
+
+function getIds(res) {
+    const ids = [];
+    
+    const movieArr = res.Search;
+    for (let i = 0; i<movieArr.length; i++){
+        ids.push(movieArr[i].imdbID);
+    }
+    return ids;
+}
+
+async function updateDOM(ids) {
+    for (let i = 0; i < ids.length; i++){
+        const response = await fetch(`http://www.omdbapi.com/?apikey=79e72e2a&type="movie"&i=${ids[i]}`);
+        const movie = await response.json();
+        if (movie.Poster !== 'N/A') {
+            addMovieToDom(movie);
+        }
+    }
+}
+
+searchBtn.addEventListener("click", async () => {
+    if (inputEl.value.length > 0) {
+        moviesEl.innerHTML = '';
+        moviesEl.style.height = '100%'; 
+
+        const search = inputEl.value;
+        const response = await fetch(`http://www.omdbapi.com/?apikey=79e72e2a&type="movie"&t=${search}&s=${search}}`);
+        const output = await response.json();
+        if (output.Response === 'True') {
+            const ids = getIds(output);
+            const movies = updateDOM(ids);
+        } else {
+            alert(output.Error);
+        }
+        
+    } else {
+        alert('Enter a movie name in the search box');
+    }
+    
 });
 
 moviesEl.addEventListener("click", (e) => {
